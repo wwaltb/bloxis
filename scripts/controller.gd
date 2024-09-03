711e9929b126af4f02a0d632f1d4589be3a02931
wwaltb <wwaltb@proton.me>
2024-09-03 15:47:49 -0700

    better movement handling with a DAS stack

--- scripts/controller.gd
+++ scripts/controller.gd
@@ -1,7 +1,14 @@
 extends Node
 
-const DAS_DELAY = 0.28
-const DAS_RATE = 0.05
+const ACTIONS_DICT: Dictionary = {
+    Vector2i.UP: "move_forward",
+    Vector2i.DOWN: "move_backward",
+    Vector2i.LEFT: "move_left",
+    Vector2i.RIGHT: "move_right",
+}
+
+const DAS_DELAY: float = 0.28
+const DAS_RATE: float = 0.05
 
 @onready var board: TileMapLayer = $Board
 @onready var active: Active = $Active
@@ -9,7 +16,7 @@ const DAS_RATE = 0.05
 var generator: PieceMachine = PieceMachine.new()
 
 var das_count: float = 0
-var das_dir: Vector2i = Vector2i.ZERO
+var das_dir_stack: Array[Vector2i]
 var das_repeat_count: float = DAS_RATE
 
 # Called when the node enters the scene tree for the first time.
@@ -37,37 +44,30 @@ func _handle_rotation_input() -> void:
 
 
 func _handle_movement_input(delta: float) -> void:
-    # reset DAS and move if any new inputs
-    if _check_just_pressed() != Vector2i.ZERO:
+    var stack_count: int = das_dir_stack.size()
+
+    # if new dir(s) on the stack move immediately and reset @das_repeat_count.
+    _check_just_pressed()
+    if das_dir_stack.size() > stack_count:
+        active.move(das_dir_stack.back())
         _reset_das()
-        das_dir = _check_just_pressed()
-        active.move(das_dir)
+        _process_das(delta)
         return
 
-    # no new inputs:
-    # reset DAS if old input is not preserved and return
-    match das_dir:
-        Vector2i.UP:
-            if not Input.is_action_pressed("move_forward"):
-                _reset_das()
-                return
-        Vector2i.DOWN:
-            if not Input.is_action_pressed("move_backward"):
-                _reset_das()
-                return
-        Vector2i.LEFT:
-            if not Input.is_action_pressed("move_left"):
-                _reset_das()
-                return
-        Vector2i.RIGHT:
-            if not Input.is_action_pressed("move_right"):
-                _reset_das()
-                return
-        _:
-            pass
+    # no new inputs.
+    # check the stack for a continued input, popping and dirs that have been lost.
+    while das_dir_stack.size() > 0:
+        if not Input.is_action_pressed(ACTIONS_DICT[das_dir_stack.back()]):
+            das_dir_stack.pop_back()
+        else:
+            break
 
-    # continued input:
-    _process_das(delta)
+    # if there are still dirs on the stack then process DAS, otherwise we have
+    # lost all inputs so reset DAS.
+    if das_dir_stack.size() > 0:
+        _process_das(delta)
+    else:
+        _reset_das()
 
 
 func _process_das(delta: float) -> void:
@@ -82,25 +82,18 @@ func _process_das(delta: float) -> void:
         return
 
     # DAS is activated and should move the piece:
-    active.move(das_dir)
+    active.move(das_dir_stack.back())
     das_repeat_count = 0
 
 
 func _reset_das() -> void:
     das_count = 0
-    das_dir = Vector2i.ZERO
     das_repeat_count = DAS_RATE
         
 
-## Checks the movement inputs for any that were just pressed. Returns the
-## vector of a just pressed direction or Vector21.ZERO if none were.
-func _check_just_pressed() -> Vector2i:
-    if Input.is_action_just_pressed("move_forward"):
-        return Vector2i.UP
-    if Input.is_action_just_pressed("move_backward"):
-        return Vector2i.DOWN
-    if Input.is_action_just_pressed("move_left"):
-        return Vector2i.LEFT
-    if Input.is_action_just_pressed("move_right"):
-        return Vector2i.RIGHT
-    return Vector2i.ZERO
+## Checks the movement inputs for any that were just pressed. Pushes any
+## directions that were to the DAS stack.
+func _check_just_pressed() -> void:
+    for dir in ACTIONS_DICT:
+        if Input.is_action_just_pressed(ACTIONS_DICT[dir]):
+            das_dir_stack.push_back(dir)
